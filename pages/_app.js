b839a1d1d094353d410c66190757c64f885d3e01
issamedine <b.issamedine@gmail.com>
2022-07-10 22:35:14 +0200

    add ract analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
 
+import ReactGA from 'react-ga'
+
+const TRACKING_ID = 'UA-233995159-1'
+
+ReactGA.initialize(TRACKING_ID)
+
 function Website({ Component, pageProps, router }) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname)
+  }, [])
+
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
